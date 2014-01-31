7eee8b848840f4d5807f9e7a9e8456957b35da96
PanicMan <PanicMan@gmx.de>
2014-01-31 04:02:07 -0800

    Finaly Works!

--- src/main.c
+++ src/main.c
@@ -1,19 +1,30 @@
 #include <pebble.h>
 
 Window *window;
-TextLayer *hhmm_layer, *ss_layer, *ddmm_layer;
+TextLayer *ddmm_layer, *yyyy_layer, *hhmm_layer, *ss_layer, *wd_layer;
 BitmapLayer *background_layer, *radio_layer;
 
 static GBitmap *background, *radio;
-static GFont digitS, digitL;
+static GFont digitS, digitM, digitL;
 
+char ddmmBuffer[] = "00-00";
+char yyyyBuffer[] = "0000";
 char hhmmBuffer[] = "00:00";
 char ssBuffer[] = "00";
-char ddmmBuffer[] = "00~00";
+char wdBuffer[] = "XXX";
+
+char *upcase(char *str) {
+    for (int i = 0; str[i] != 0; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] -= 'a' - 'A';
+        }
+    }
+    return str;
+}
 
 void bluetooth_connection_handler(bool connected)
 {
-	//layer_set_hidden(bitmap_layer_get_layer(radio_layer), connected != true);
+	layer_set_hidden(bitmap_layer_get_layer(radio_layer), connected != true);
 }
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
@@ -32,8 +43,16 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 		
 		text_layer_set_text(hhmm_layer, hhmmBuffer);
 		
-		strftime(ddmmBuffer, sizeof(ddmmBuffer), "%d %m", tick_time);
+		strftime(ddmmBuffer, sizeof(ddmmBuffer), "%d-%m", tick_time);
 		text_layer_set_text(ddmm_layer, ddmmBuffer);
+		
+		strftime(wdBuffer, sizeof(wdBuffer), "%a", tick_time);
+		//strcpy(wdBuffer, "Mon");
+		upcase(wdBuffer);
+		text_layer_set_text(wd_layer, wdBuffer);
+
+		strftime(yyyyBuffer, sizeof(yyyyBuffer), "%Y", tick_time);
+		text_layer_set_text(yyyy_layer, yyyyBuffer);
 	}
 }
 
@@ -49,38 +68,55 @@ void window_load(Window *window)
 	layer_add_child(window_layer, bitmap_layer_get_layer(background_layer));
 	
 	digitS = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_RESOURCE_ID_FONT_DIGITAL_25));
+	digitM = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_RESOURCE_ID_FONT_DIGITAL_35));
 	digitL = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_RESOURCE_ID_FONT_DIGITALTHIN_55));
   
 	//DAY+MONTH layer
-	ddmm_layer = text_layer_create(GRect(3, 5, 35, 37));
+	ddmm_layer = text_layer_create(GRect(2, 5, 70, 32));
 	text_layer_set_background_color(ddmm_layer, GColorClear);
 	text_layer_set_text_color(ddmm_layer, GColorBlack);
 	text_layer_set_text_alignment(ddmm_layer, GTextAlignmentCenter);
 	text_layer_set_font(ddmm_layer, digitS);
 	layer_add_child(window_layer, text_layer_get_layer(ddmm_layer));
         
+	//YEAR layer
+	yyyy_layer = text_layer_create(GRect(82, 5, 60, 32));
+	text_layer_set_background_color(yyyy_layer, GColorClear);
+	text_layer_set_text_color(yyyy_layer, GColorBlack);
+	text_layer_set_text_alignment(yyyy_layer, GTextAlignmentCenter);
+	text_layer_set_font(yyyy_layer, digitS);
+	layer_add_child(window_layer, text_layer_get_layer(yyyy_layer));
+        
 	//HOUR+MINUTE layer
-	hhmm_layer = text_layer_create(GRect(0, 50, 100, 125));
-	text_layer_set_background_color(hhmm_layer, GColorBlack);
-	text_layer_set_text_color(hhmm_layer, GColorWhite);
+	hhmm_layer = text_layer_create(GRect(0, 50, 110, 75));
+	text_layer_set_background_color(hhmm_layer, GColorClear);
+	text_layer_set_text_color(hhmm_layer, GColorBlack);
 	text_layer_set_text_alignment(hhmm_layer, GTextAlignmentCenter);
 	text_layer_set_font(hhmm_layer, digitL);
-	//layer_add_child(window_layer, text_layer_get_layer(hhmm_layer));
+	layer_add_child(window_layer, text_layer_get_layer(hhmm_layer));
         
 	//SECOND layer
-	ss_layer = text_layer_create(GRect(100, 5, 140, 37));
-	text_layer_set_background_color(ss_layer, GColorBlack);
-	text_layer_set_text_color(ss_layer, GColorWhite);
+	ss_layer = text_layer_create(GRect(111, 55, 30, 30));
+	text_layer_set_background_color(ss_layer, GColorClear);
+	text_layer_set_text_color(ss_layer, GColorBlack);
 	text_layer_set_text_alignment(ss_layer, GTextAlignmentCenter);
 	text_layer_set_font(ss_layer, digitS);
 	layer_add_child(window_layer, text_layer_get_layer(ss_layer));
         
+	//WEEKDAY layer
+	wd_layer = text_layer_create(GRect(3, 125, 84, 40));
+	text_layer_set_background_color(wd_layer, GColorClear);
+	text_layer_set_text_color(wd_layer, GColorBlack);
+	text_layer_set_text_alignment(wd_layer, GTextAlignmentCenter);
+	text_layer_set_font(wd_layer, digitM);
+	layer_add_child(window_layer, text_layer_get_layer(wd_layer));
+        
 	//Init bluetooth radio
 	radio = gbitmap_create_with_resource(RESOURCE_ID_RESOURCE_ID_IMAGE_RADIO);
-	radio_layer = bitmap_layer_create(GRect(111, 133, 144, 168)); //31x33
+	radio_layer = bitmap_layer_create(GRect(106, 130, 31, 33)); //31x33
 	bitmap_layer_set_background_color(radio_layer, GColorClear);
 	bitmap_layer_set_bitmap(radio_layer, radio);
-	//bitmap_layer_set_compositing_mode(radio_layer, GCompOpAnd);
+	bitmap_layer_set_compositing_mode(radio_layer, GCompOpAnd);
 	layer_add_child(window_layer, bitmap_layer_get_layer(radio_layer));
 
 	//Get a time structure so that it doesn't start blank
@@ -98,11 +134,14 @@ void window_unload(Window *window)
 {
 	//Destroy text layers
 	text_layer_destroy(ddmm_layer);
-	text_layer_destroy(ss_layer);
+	text_layer_destroy(yyyy_layer);
 	text_layer_destroy(hhmm_layer);
+	text_layer_destroy(ss_layer);
+	text_layer_destroy(wd_layer);
 	
 	//Unload Fonts
 	fonts_unload_custom_font(digitS);
+	fonts_unload_custom_font(digitM);
 	fonts_unload_custom_font(digitL);
 	
 	//Destroy GBitmaps
